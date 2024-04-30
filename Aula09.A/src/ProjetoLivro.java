ce708fcaa8a5833b3c2acbee9942530d2f1ee8c8
luiz <lh626394@gmail.com>
2024-04-30 18:16:05 -0300

    Exercicio 09

--- Aula09.A/src/ProjetoLivro.java
+++ Aula09.A/src/ProjetoLivro.java
@@ -1,7 +1,22 @@
+import classes.Livro;
+import classes.Pessoa;
+
 public class ProjetoLivro {
     public static void main(String[] args) throws Exception {
-        Pessoa[] p = new Pessoa[2]
-        Livro[] l = new Livro[3]
+        Pessoa[] p = new Pessoa[2];
+        Livro [] l = new Livro[2];
+
+        p[0] = new Pessoa("Luiz", 17, "M");
+        p[1] = new Pessoa("Larah", 15, "F");
+
+        l[0] = new Livro("Aprendendo Java", "Roberto", 900, 0, false, p[0]);
+        l[1] = new Livro("Aprendendo POO", "Ezequias", 700, 10, false,p[1]);
+        
+        l[0].detalhes();
+        l[0].avancarPag();
+        
+        
+
         
     }
 }
