0c8e816e78883b97a2ee02d4d4937dbee83f78d4
LUIZ HENRIQUE SANTANA MOUSINHO DA SILVA <13507847450@poli.local>
2024-05-07 11:35:36 -0300

    Exercicio aula 12

---
+++ Aula12.A/src/Classes/Repitil.java
@@ -0,0 +1,24 @@
+public class Repitil extends Animal {
+   
+    private String corEscama;
+    
+    @Override
+    public void locomover(){
+        System.out.println("Rastejando");
+    }
+    @Override
+    public void alimentar(){
+        System.out.println("Comendo Vegetais");
+    }
+    @Override
+    public void emitirSom(){
+        System.out.println("som de Réptil");
+    }
+    public String getCorEscama(){
+        return this.corEscama;
+    }
+    public void setCorEscama(String corEscama){
+        this.corEscama = corEscama;
+    }
+   
+}
