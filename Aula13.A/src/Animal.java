3ff9dca81964b934087f7529514ca2f2f6216d19
LUIZ HENRIQUE SANTANA MOUSINHO DA SILVA <13507847450@poli.local>
2024-05-08 10:16:32 -0300

    Exercicio aula 13

---
+++ Aula13.A/src/Animal.java
@@ -0,0 +1,10 @@
+public abstract class  Animal {
+   
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    public abstract void locomover();
+    public abstract void emitirSom();
+    
+}
---
+++ Aula13.A/src/Lobo.java
@@ -0,0 +1,15 @@
+public class Lobo extends Animal{
+   
+    private String corPelo;
+
+    @Override 
+    public void locomover(){
+        System.out.println("Correndo");
+    }
+    @Override
+    public void emitirSom(){
+        System.out.println("Auuuuuuuuuuuuuuuuuuuuu!");
+    } 
+
+
+}
